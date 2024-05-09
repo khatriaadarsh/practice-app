114fa50f431d7a3f2ebc522a5fa6f92e319b0e25
khatriaadarsh <aadarshkumark2003@gmail.com>
2024-05-09 14:00:34 +0500

    Add card in new file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+// import { Link } from 'react-router-dom';
+import Card from './Components/Card'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <Card />
     </div>
-  );
+
+    </>
+  )
 }
 
-export default App;
+// const h1_Virtual = <h2>This is the h1 of Virtual Dom</h2>
+// console.log(h1_Virtual)
+// const h1_Real = document.createElement('h1');
+// h1_Real.innerText = 'This is the h1 of Real dom';
+// console.log(h1_Real) 
+
+export default App
